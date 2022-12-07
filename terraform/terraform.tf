9df528ad633ff505ba9af4ce6a2108cbc16717c2
Warren Krewenki <19960+krewenki@users.noreply.github.com>
2022-12-07 11:49:08 -0400

    Blank slate with remote state

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "krewenki-tfstate"
+    key            = "sites/warrenkrewenki.com/terraform.tfstate"
+    region         = "ca-central-1"
+    encrypt        = true
+    dynamodb_table = "krewenki-tfstate-lock"
+  }
 }
