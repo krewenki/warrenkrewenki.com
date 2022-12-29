c678c4147257a82547a53007fc73fd7ddaad0144
Warren Krewenki <19960+krewenki@users.noreply.github.com>
2022-12-29 07:42:05 -0400

    A bit of organization before using more modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,49 +14,4 @@ resource "aws_kms_key" "objects" {
   deletion_window_in_days = 7
 }
 
-module "s3_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.6.0"
 
-  bucket = local.bucket_name
-
-
-
-  acl           = "private" # "acl" conflicts with "grant" and "owner"
-  force_destroy = true
-
-}
-
-data "aws_iam_policy_document" "s3_policy" {
-  version = "2012-10-17"
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
-    principals {
-      type        = "AWS"
-      identifiers = module.cloudfront.cloudfront_origin_access_identity_iam_arns
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "docs" {
-  bucket = module.s3_bucket.s3_bucket_id
-  policy = data.aws_iam_policy_document.s3_policy.json
-}
-
-resource "aws_acm_certificate" "ssl_certificate" {
-  provider                  = aws.acm_provider
-  domain_name               = local.domain_name
-  subject_alternative_names = ["*.${local.domain_name}"]
-  validation_method         = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "cert_validation" {
-  provider                = aws.acm_provider
-  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
