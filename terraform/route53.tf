3fbcf1745db251de16423c96a60bbeccded300ab
Warren Krewenki <19960+krewenki@users.noreply.github.com>
2023-06-17 12:26:55 -0300

    Setting up github pages dns (#5)

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,41 +2,33 @@ resource "aws_route53_zone" "domain" {
   name = local.domain_name
 }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.domain.zone_id
-}
-
-resource "aws_route53_record" "root" {
-  name = local.domain_name
-  #records = [module.cloudfront.cloudfront_distribution_domain_name]
-  type    = "A"
-  zone_id = aws_route53_zone.domain.zone_id
-  alias {
-    name                   = module.cloudfront.cloudfront_distribution_domain_name
-    zone_id                = module.cloudfront.cloudfront_distribution_hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "www" {
-  name    = "www"
-  records = [module.cloudfront.cloudfront_distribution_domain_name]
+resource "aws_route53_record" "github_pages_verification" {
+  name    = "_github-pages-challenge-krewenki"
   ttl     = 60
-  type    = "CNAME"
+  records = ["3ae64d1c905aac3b15aaa6abf6663a"]
+  type    = "TXT"
   zone_id = aws_route53_zone.domain.zone_id
 }
 
+resource "aws_route53_record" "github_pages_www" {
+  name    = "www"
+  type    = "CNAME"
+  ttl     = 60
+  records = ["krewenki.github.io"]
+  zone_id = aws_route53_zone.domain.zone_id
+}
+
+resource "aws_route53_record" "github_pages_root" {
+  name = ""
+  type = "A"
+  ttl  = 60
+  records = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153"
+  ]
+  zone_id = aws_route53_zone.domain.zone_id
+}
 
